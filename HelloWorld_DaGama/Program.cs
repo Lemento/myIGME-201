86c8374be005b03301d47c08fe91605033fe6943
Lemento <dagai89@gmail.com>
2020-08-22 20:01:57 -0700

    First Assignment

---
+++ HelloWorld_DaGama/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_DaGama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm Phillip, and I can count to ten");
+
+            for (int x = 1; x <= 10; x++)
+            {
+                Console.WriteLine("x is {0}", x);
+            }
+        }
+    }
+}
