83575b69b6db7eb5e4380a3b384b206a66627a2e
Lemento <dagai89@gmail.com>
2020-08-31 00:13:57 -0700

    Fixed legibility

--- PE3 - Question5/Program.cs
+++ PE3 - Question5/Program.cs
@@ -10,20 +10,26 @@ namespace PE3__5
     {
         static void Main(string[] args)
         {
+            // declare variables for user input and their product
             int var1, var2, var3, var4;
             int resultVar;
 
+            // For each variable, ask user to input the value to be multiplied with the others
             Console.WriteLine("Please input first value...");
-            var1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please input second value...");
-            var2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please input third value...");
-            var3 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please input fourth value...");
+
+            // Proceed to then convert each user inputted string into an integer for multiplication
+            var1 = Convert.ToInt32(Console.ReadLine());
+            var2 = Convert.ToInt32(Console.ReadLine());
+            var3 = Convert.ToInt32(Console.ReadLine());
             var4 = Convert.ToInt32(Console.ReadLine());
 
+            // Give console visual of all the variables they had inputted to be multiplied
             Console.WriteLine("var1: " + var1 + ", var2: " + var2 + ", var3: " + var3 + ", var4: " + var4);
 
+            // make resultVar equal to the product of all the variables and output it to the console
             resultVar = (var1 * var2 * var3 * var4);
             Console.WriteLine("The product is: " + resultVar);
         }
