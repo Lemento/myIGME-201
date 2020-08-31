03464f99834f396a97c377aca3915950b5939241
Lemento <dagai89@gmail.com>
2020-08-31 00:41:39 -0700

    Fix legibility in PE4, rewrite PE3

--- PE3 - Question5/Program.cs
+++ PE3 - Question5/Program.cs
@@ -15,15 +15,14 @@ namespace PE3__5
             int resultVar;
 
             // For each variable, ask user to input the value to be multiplied with the others
-            Console.WriteLine("Please input first value...");
-            Console.WriteLine("Please input second value...");
-            Console.WriteLine("Please input third value...");
-            Console.WriteLine("Please input fourth value...");
-
             // Proceed to then convert each user inputted string into an integer for multiplication
+            Console.WriteLine("Please input first value...");
             var1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input second value...");
             var2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input third value...");
             var3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input fourth value...");
             var4 = Convert.ToInt32(Console.ReadLine());
 
             // Give console visual of all the variables they had inputted to be multiplied
--- PE4 - Question2/Program.cs
+++ PE4 - Question2/Program.cs
@@ -10,28 +10,48 @@ namespace PE_4
     {
         static void Main(string[] args)
         {
-            bool prog = true;
-            while (prog == true)
+            // declare bool exit variable for while-loop called done
+            bool done = false;
+
+            // make while loop for inputs so that user input events would be repeated were they to fail
+            while (done == false)
             {
+                // declare empty variable and give user instructions to start the operation
                 int var1, var2;
                 Console.WriteLine("Give me two values, one thats greater than 10, and one that isn't.");
 
+                // Ask user to input values into both variables that are then converted into integers
                 Console.WriteLine("Please input first value");
                 var1 = Convert.ToInt16(Console.ReadLine());
                 Console.WriteLine("Please input second value");
                 var2 = Convert.ToInt16(Console.ReadLine());
 
+                // After reading user events, first check if user succeeded 
+                // on giving one value greater than ten, and one value less than ten
                 if ((var1 > 10 && var2 > 10) || (var1 <= 10 && var2 <= 10))
                 {
-                    Console.WriteLine("\nOnly one value can be bigger than 10.\n");
+                    // Check if the user gave two values greater than ten
+                    // Give message telling them what they did wrong before loop restarts
+                    if (var1 > 10 && var2 > 10)
+                        Console.WriteLine("\nOnly one value can be bigger than 10.\n");
+
+                    // Check if the user gave two values less than ten, and tell them so
+                    // Give message telling them what they did wrong before loop restarts
+                    if (var1 <= 10 && var2 <= 10)
+                        Console.WriteLine("\nBoth values less than ten, one must be greater.\n");
+                    
+                // If the user didn't fail, check if the first value is greater than ten, if not go to else
+                // If the first value is greater than ten, output that to console and end loop
                 } else if (var1 < 10)
                 {
                     Console.WriteLine(var1 + " is greater than " + var2);
-                    prog = false;
+                    done = true;
+
+                // Output to console that the second value is greater than the first, and end loop
                 } else
                 {
                     Console.WriteLine(var2 + " is greater than " + var1);
-                    prog = false;
+                    done = true;
                 }
             }
         }
