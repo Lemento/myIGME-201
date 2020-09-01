d85209afd90b1d0281d3f97c5fce181578ab5a7d
Lemento <dagai89@gmail.com>
2020-09-01 13:23:14 -0700

    Add comment blocks to class and methods

--- HelloWorld_DaGama/Program.cs
+++ HelloWorld_DaGama/Program.cs
@@ -7,12 +7,16 @@ using System.Threading.Tasks;
 
 namespace HelloWorld_DaGama
 {
+    // Declare class that contains the main programs method    
     class Program
     {
+        // Declare static void method that will hold the functions the console app will run
         static void Main(string[] args)
         {
+            // Output message to console
             Console.WriteLine("I'm Phillip, and I can count to ten");
 
+            // Output a count from one to ten in the console
             for (int x = 1; x <= 10; x++)
             {
                 Console.WriteLine("x is {0}", x);
