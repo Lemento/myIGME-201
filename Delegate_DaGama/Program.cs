14cfc8c128a0a2aebea8e00a8e783c1a379718c8
Lemento <dagai89@gmail.com>
2020-09-13 02:03:41 -0700

    Added PE9 Question 3

--- Delegate_DaGama/Program.cs
+++ Delegate_DaGama/Program.cs
@@ -6,24 +6,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Delegate_DaGama
-{   /// Class: Program
+{
+    class Program
+    /// Class: Program
     /// Author: Phillip Da Gama Paes
     /// Purpose: Contains anser to PE9, Question 3
     /// Restrictions: none
-    class Program
     {
         // the definition of the delegate function data type
         public delegate void DelM();
-
-        // Method: Main
-        // Purpose: Impersonate the ReadLine Function with a delegate function
-        // Restrictions: Can only take user input and echo it
 
         public static void Message()
         {
             Console.WriteLine(Console.ReadLine());
         }
         static void Main(string[] args)
+        // Method: Main
+        // Purpose: Impersonate the ReadLine Function with a delegate function
+        // Restrictions: Can only take user input and echo it
+
         {
             Console.WriteLine("Input message.");
             DelM output = Message;
