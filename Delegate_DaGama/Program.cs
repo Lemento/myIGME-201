a3be3870b2868fd26f80178ad7f39d67b557f33a
Lemento <dagai89@gmail.com>
2020-09-13 01:58:36 -0700

    Added PE9 Question 3

---
+++ Delegate_DaGama/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_DaGama
+{   /// Class: Program
+    /// Author: Phillip Da Gama Paes
+    /// Purpose: Contains anser to PE9, Question 3
+    /// Restrictions: none
+    class Program
+    {
+        // the definition of the delegate function data type
+        public delegate void DelM();
+
+        // Method: Main
+        // Purpose: Impersonate the ReadLine Function with a delegate function
+        // Restrictions: Can only take user input and echo it
+
+        public static void Message()
+        {
+            Console.WriteLine(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input message.");
+            DelM output = Message;
+            output();
+        }
+    }
+}
