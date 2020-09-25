faa95b309133c78edf5d6a95d4f7ab829ad33a80
Lemento <dagai89@gmail.com>
2020-09-25 00:41:57 -0400

    Added PE12 Question 3

---
+++ PE12 - Question3/PE12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12
+{
+    public class MyClass
+    {
+        private string myString = "myString";
+        public virtual string GetString()
+        {
+            return myString;
+        }
+
+        public string MyString
+        {
+            set { myString = value; }
+        }
+    }
+
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            return base.GetString() + " (output from the derived class)";
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyDerivedClass derived = new MyDerivedClass();
+
+            Console.WriteLine(derived.GetString());
+        }
+    }
+}
